6b46c795d67c8c44d0bcbde88663f409fb664f7a
Quang Huy <quanghuyy150604@gmail.com>
2023-09-18 20:21:11 +0700

    Week 3

---
+++ Week 3/HW7-3-SumFast/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import edu.princeton.cs.algs4.*;
+
+public class Main {
+    public static int binarySearch(int[] a, int number) {
+        int left = 0;
+        int right = a.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (number == a[mid])
+                return mid;
+            else if (number < a[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        In in = new In("D:\\Codeeeee\\algs4-data\\1Kints.txt");
+        int[] a = in.readAllInts();
+
+        Arrays.sort(a);
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                int check = binarySearch(a, -(a[i] + a[j]));
+                if (check != -1)
+                    System.out.println(a[i] + " " + a[j] + " " + a[check]);
+            }
+        }
+
+    }
+}
